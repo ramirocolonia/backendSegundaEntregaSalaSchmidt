6e9d2069a8dd5c5da69ae1192acdf56d0bfc21d7
Ramiro Sala <ramiro.sala85@gmail.com>
2024-02-28 17:50:25 -0300

    no funciona handlebars de cart list

---
+++ src/routes/views.router.js
@@ -0,0 +1,38 @@
+import {Router} from "express";
+
+import ProductManagerDB from "../dao/ProductManagerDB.js";
+import CartManagerDB from "../dao/CartManagerDB.js";
+
+const viewsRouter = Router();
+
+viewsRouter.get("/api/chat", (req,res)=>{
+  res.render("chat", {})
+});
+
+viewsRouter.get('/products',async (req,res)=>{
+  try {
+    const prodManager = new ProductManagerDB();
+    const resp = await prodManager.loadProducts(req.query);
+    resp.payload.prevLink = resp.payload.hasPrevPage?`http://localhost:8080/products?page=${resp.payload.prevPage}&limit=${resp.payload.limit}`:"";
+    resp.payload.nextLink = resp.payload.hasNextPage?`http://localhost:8080/products?page=${resp.payload.nextPage}&limit=${resp.payload.limit}`:"";
+    resp.payload.isValid = (resp.payload.docs.length > 0);
+    console.log(resp.payload);
+    res.render("products",resp.payload)
+  } catch (error) {
+    res.send({status: "error", message: "Error en ejecución, " + error});
+  }
+});
+
+viewsRouter.get('/carts/:cid',async (req,res)=>{
+  try {
+    const cartManager = new CartManagerDB();
+    const resp = await cartManager.loadCart(req.params.cid);
+    const products = resp.payload[0].products;
+    console.log(products);
+    res.render("cartList", products)
+  } catch (error) {
+    res.send({status: "error", message: "Error en ejecución, " + error});
+  }
+});
+
+export default viewsRouter;
